ba831c82642d92b86a57b7161e4a5950c19a222f
Bruce SIBANYONI <bsibanyo@c5r5s10.wethinkcode.co.za>
2019-05-21 14:14:35 +0200

    ft_strncpy.c

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,23 +6,22 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 13:52:59 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/05/20 14:58:45 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/05/21 14:14:09 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-char	*ft_strncpy(char * dst, const char * src, size_t len)
+char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
 	unsigned int		i;
 
 	i = 0;
-	while(src[i] != '\0' && (i < len))
+	while (src[i] != '\0' && (i < len))
 	{
 		dst[i] = src[i];
 		i++;
 	}
-
 	dst[i] = '\0';
-	return(dst);
+	return (dst);
 }
