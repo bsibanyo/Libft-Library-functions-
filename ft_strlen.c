387ec79d9d9f323f78e87c4c18a1987088e3af4b
Bruce SIBANYONI <bsibanyo@c5r5s10.wethinkcode.co.za>
2019-05-21 14:00:10 +0200

    ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 10:22:31 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/05/20 13:45:07 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/05/21 13:59:11 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ size_t	strlen(const char *s)
 	int		i;
 
 	i = 0;
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
-	return(i);
+	return (i);
 }
