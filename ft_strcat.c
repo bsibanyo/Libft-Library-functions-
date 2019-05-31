b569f635fd8c8843826f46d31586085cdfb1371a
Bruce SIBANYONI <bsibanyo@c5r5s10.wethinkcode.co.za>
2019-05-31 11:51:36 +0200

    ft_strcat.c

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,21 +6,28 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 15:03:21 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/05/21 14:12:06 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/05/31 11:46:28 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
-char	*ft_strcat(char *restrict s1, const char *restrict s2)
+char	*ft_strcat(char *s1, const char *s2)
 {
-	int		i;
+	unsigned int		i;
+	unsigned int		j;
 
 	i = 0;
-	while (s2[i] != '\0')
+	j = 0;
+	while (s1[i] != '\0')
 	{
-		s1[i] = s2[i];
 		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		s1[i] = s2[j];
+		i++;
+		j++;
 	}
 	s1[i] = '\0';
 	return (s1);
