fc7ef80198448b5475a77cf74b2a8b63e012e4ce
Bruce SIBANYONI <bsibanyo@c5r5s10.wethinkcode.co.za>
2019-05-31 11:41:54 +0200

    ft_strcat.c

---
+++ test/ft_strcat.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/31 11:40:53 by bsibanyo          #+#    #+#             */
+/*   Updated: 2019/05/31 11:41:00 by bsibanyo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strcat(char *s1, const char *s2)
+{
+	unsigned int		i;
+	unsigned int		j;
+
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		s1[i] = s2[j];
+		i++;
+		j++;
+	}
+	s1[i] = '\0';
+	return (s1);
+}
+
+int		main(void)
+{
+	char name1[20] = "This is ";
+	const char name2[20] = "me";
+
+	ft_strcat(name1, name2);
+
+	printf("%s", name1);
+	return (0);
+}
