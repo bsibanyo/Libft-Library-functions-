64670e2ecc2be354f47343043e4ac97dffed61c8
Bruce SIBANYONI <bsibanyo@c5r5s10.wethinkcode.co.za>
2019-05-31 11:28:03 +0200

    ft_toupper.c

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,11 +6,11 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 15:56:59 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/05/21 14:11:05 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/05/31 11:27:49 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 int		ft_toupper(int c)
 {
