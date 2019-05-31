4130bfa68883cac97a8d6877f4551eef93ae8016
Bruce SIBANYONI <bsibanyo@c5r5s10.wethinkcode.co.za>
2019-05-31 11:19:20 +0200

    ft_isdigit.c

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,11 +6,11 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 15:17:55 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/05/24 08:32:31 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/05/31 11:19:04 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 int		ft_isdigit(int c)
 {
