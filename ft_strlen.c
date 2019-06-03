3c8dc4e3ec75ed11001d5aeb714ffb3573043bfe
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-03 17:24:37 +0200

    ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,13 +6,13 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 10:22:31 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/05/31 11:25:54 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/03 17:23:09 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
 	int		i;
 
