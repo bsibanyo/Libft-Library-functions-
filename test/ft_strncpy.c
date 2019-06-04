0ec433c8517a034397eff25c53acdefe42183c56
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-04 14:40:27 +0200

    ft_strncpy.c

---
+++ test/ft_strncpy.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/04 14:22:45 by bsibanyo          #+#    #+#             */
+/*   Updated: 2019/06/04 14:39:53 by bsibanyo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strncpy(char *dst, const char *src, size_t len)
+{
+	unsigned int		i;
+
+	i = 0;
+	while (src[i] != '\0' && (i < len))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+int		main()
+{
+	const char	a[50] = "Hello";
+	char	b[50];
+
+	ft_strncpy(b,a,3);
+	printf("%s", b);
+	return (0);
+}
