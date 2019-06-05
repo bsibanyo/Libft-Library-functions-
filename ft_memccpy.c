220bcd2310697067ef7b742cbdbedcf5ca07a0a1
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-05 16:47:11 +0200

    ft_memccpy.c

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,11 +6,11 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 15:31:22 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/05 15:55:26 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/05 16:46:49 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libt.h"
+#include "libft.h"
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
