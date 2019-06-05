3742c82add8775085d764b346539d7bd822d30ec
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-05 14:19:56 +0200

    ft_memcmp.c

--- test/ft_memcmp.c
+++ test/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 10:00:30 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/05 10:09:48 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/05 14:19:05 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,35 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	int		i;
+	char	*str1;
+	char	*str2;
+	size_t		i;
 
 	i = 0;
-	while(s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+	str1 = (char*)s1;
+	str2= (char*)s2;
+	while (i < n)
 	{
-		i++;
+		if (str1[i] != str2[i])
+		{
+			return (str1[i] - str2[i]);
+			i++;
+		}
 	}
-	return (s1[i] - s2[i] *sizeof(int));
+	return(0);
 }
 
 int		main()
 {
-	char a = "hello";
-	char b = "he";
-	int		d;
+	int		i;
+	int		j;
 
-	d = ft_memcmp(a,b,2);
-	if(d == 0)
-	{
-		printf("%c equal to %c",a,b);
-	}
-	else if(d > 0)
-	{
-		printf("%c is greater than %c",a,b);
-	}
-	else if(d < 0)
-	{
-		printf("%c is less than %c",a,b);
-	}
+	const void (*fptr)(int,int);
+
+	fptr = ft_memcmp;
+
+	void	sum = (*fptr)(2,3);
+
+	printf("%d",fptr);
 	return (0);
 }
