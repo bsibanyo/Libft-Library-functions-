f7cfbe2aca04cd520619b0260c84888d7a4138e7
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-05 15:00:58 +0200

    ft_strlcat.c

---
+++ ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/05 13:46:25 by bsibanyo          #+#    #+#             */
+/*   Updated: 2019/06/05 15:00:39 by bsibanyo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t		i;
+	size_t		j;
+
+	i = 0;
+	j = 0;
+	while(dst[i] != '\0')
+	{
+		i++;
+	}
+	while(src[j] != '\0' && (j > dstsize))
+	{
+		if(dst[i] == '\0')
+		{
+			ft_putchar(src[j]);
+		}
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = src[j];
+	return(*dst);
+}
