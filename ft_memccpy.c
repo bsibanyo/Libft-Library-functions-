612f4bfc4a3e55571bb5cf94183810eaee46f2a6
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-05 15:56:26 +0200

    ft_memccpy.c

---
+++ ft_memccpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/05 15:31:22 by bsibanyo          #+#    #+#             */
+/*   Updated: 2019/06/05 15:55:26 by bsibanyo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libt.h"
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	size_t	i;
+	char	*d;
+	char	*s;
+
+	i = 0;
+	d = (char*)dst;
+	s = (char*)src;
+	while (i < n)
+	{
+		d[i] = s[i];
+		if ((unsigned char)s[i] == (unsigned char)c)
+		{
+			return ((char*)dst + i + 1);
+			i++;
+		}
+	}
+	return (NULL);
+}
