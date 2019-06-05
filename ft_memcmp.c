9c94e27214583b213ccbf7ee6785fa02226e0f68
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-05 14:31:17 +0200

    ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,8 +6,28 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 09:59:34 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/05 10:00:02 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/05 14:30:52 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	char	*str1;
+	char	*str2;
+	size_t	i;
+
+	i = 0;
+	str1 = (char*)s1;
+	str2 = (char*)s2;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+		{
+			return (str1[i] - str2[i]);
+			i++;
+		}
+	}
+	return (0);
+}
