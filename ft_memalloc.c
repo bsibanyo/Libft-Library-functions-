3dd3a7cb6672cbde0150b60fdc4ea6d5cb7c5c25
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-10 16:44:41 +0200

    ft_memalloc.c

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,19 +6,19 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 15:02:40 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/10 15:38:19 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/10 16:44:20 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memalloc(size_t n)
+void	*ft_memalloc(size_t size)
 {
-	mem = malloc(n);
+	mem = malloc(size);
 	if (mem == NULL)
 	{
 		return (NULL);
 	}
-	ft_bzero(mem, n);
+	ft_bzero(mem, size);
 	return (mem);
 }
