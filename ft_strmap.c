85c64c39a484e53382bbbb1110e5c1bf21f50358
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-10 17:28:37 +0200

    ft_strmap.c

---
+++ ft_strmap.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/10 16:37:47 by bsibanyo          #+#    #+#             */
+/*   Updated: 2019/06/10 17:26:24 by bsibanyo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	unsigned int	i;
+    char			*str;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s)) + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (s[i] != '\0')
+	{
+		str[i] = f(s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
