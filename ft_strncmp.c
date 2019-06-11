7f4062f9bad8bcfeb249df6313324b100f3c6f36
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-11 15:34:36 +0200

    ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 14:27:41 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/05 10:24:14 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/11 15:34:06 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,19 @@
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned int		i;
+	unsigned char		*ptr1;
+	unsigned char		*ptr2;
 
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && (n > i))
+	ptr1 = (unsigned char*)s1;
+	ptr2 = (unsigned char*)s2;
+	while (ptr1[i] == ptr2[i] && ptr1[i] != '\0' && ptr2[i] != '\0' && (n - 1 > i))
 	{
 		i++;
-		n--;
 	}
-	return (s1[i] - s2[i]);
+	if(n == 0)
+	{
+		return(0);
+	}
+	return (ptr1[i] - ptr2[i]);
 }
