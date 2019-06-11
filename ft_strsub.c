94749e39d24825bccaceded855a1b3ee7309d07d
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-11 16:46:39 +0200

    ft_strsub.c

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 16:38:44 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/11 16:26:03 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/11 16:46:20 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,19 @@
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
 	unsigned int	i;
-   char				*str;
-   
-   i = 0;
-   str = (char *)malloc(sizeof(char)* len + 1);
-   if (str == NULL)
-	   return (NULL);
-   while (i < len)
-   {
-	   str[i] = s[start + i];
-	   i++;
-   }
-   str[i] = '\0';
-   return (str);
+	char			*str;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (i < len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
