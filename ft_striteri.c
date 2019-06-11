b26b90b2304687bee5354e93b3d5e7c1aa292991
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-11 09:53:37 +0200

    ft_striteri.c

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,13 +6,13 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 16:37:31 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/11 09:47:47 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/11 09:51:31 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_striteri(char *s, void (*f)(char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	i;
 
