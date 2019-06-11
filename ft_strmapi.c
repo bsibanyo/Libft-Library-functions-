268a12a567c70f83a3802723703970d526676aa9
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-11 10:13:48 +0200

    ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,8 +6,28 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 16:38:04 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/10 17:40:24 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/11 10:13:27 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int		i;
+	char				*str;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s)) + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (s[i] != '\0')
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
