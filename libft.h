e0a5fe87e56d25c1d22ff7a097ea22936dba4e43
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-11 09:43:27 +0200

    Makefile

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 16:46:50 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/11 09:38:50 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/11 09:42:52 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ void				ft_strdel(char **as);
 void				ft_strclr(char *s);
 void				ft_striter(char *s, void (*f)(char*));
 void				*ft_memmove(void *dst, const void *src, size_t len);
-void				ft_striter(char *s, void (*f)(char*));
+void				ft_striteri(char *s, void (*f)(char*));
 char				*ft_strmap(char const *s, char (*f)(char));
 
 
