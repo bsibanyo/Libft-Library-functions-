beed5c70e4188fc2e9dcf2b8507f14636a5481eb
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-11 11:00:16 +0200

    ft_strequ.c

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,7 +6,13 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 16:38:16 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/10 17:06:04 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/11 10:59:59 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int		ft_strequ(char const *s1, char const *s2)
+{
+	return (ft_strcmp(s1, s2) ? 0 : 1);
+}
