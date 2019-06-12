0fce3c7bd93731a06c07a95a4eb3e729e586e6de
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-12 17:05:23 +0200

    ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 13:46:25 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/06 09:18:23 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/12 17:04:57 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,26 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t		i;
-	size_t		j;
+	unsigned int		i;
+	unsigned int		j;
+	unsigned int		k;
 
 	i = 0;
 	j = 0;
+	k = 0;
 	while (dst[i] != '\0')
 	{
 		i++;
 	}
-	while (src[j] != '\0' && (j > dstsize))
+	while (src[j] != '\0' && j < dstsize)
 	{
-		if (dst[i] == '\0')
-		{
-			ft_putchar(src[j]);
-		}
-		dst[i] = src[j];
-		i++;
+		dst[i + j] = src[j];
 		j++;
 	}
-	dst[i] = src[j];
-	return (*dst);
+	dst[i + j] = '\0';
+	while (src[k] != '\0')
+	{
+		k++;
+	}
+	return (k + dstsize);
 }
