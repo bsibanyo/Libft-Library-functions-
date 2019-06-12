42f5e8b59d4d7eb205caf058249267be135a182f
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-12 17:37:19 +0200

    ./ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,8 +6,32 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 16:49:23 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/11 16:49:25 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/12 17:36:54 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strrchr(const char *s, int c)
+{
+	char	*str;
+
+	str = 0;
+	while (*s)
+	{
+		if (*s == c)
+		{
+			str = (char *)s;
+			++s;
+		}
+	}
+	if (str)
+	{
+		return (str);
+	}
+	if (c == '\0')
+	{
+		return ((char *)s);
+	}
+	return (0);
+}
