dc716a829cabd59022756029e66ec143c38d4edf
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-13 15:13:51 +0200

    ft_strequ.c

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 16:38:16 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/11 10:59:59 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/13 15:13:07 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 int		ft_strequ(char const *s1, char const *s2)
 {
-	return (ft_strcmp(s1, s2) ? 0 : 1);
+	if (!s1)
+		return ('\0');
+	if (ft_strcmp(s1,s2))
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
