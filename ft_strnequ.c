614cbfa0ed5d7733a4825d7a86f5cb55d9d3efd0
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-13 16:03:50 +0200

    ft_strnequ.c

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 16:38:30 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/13 15:14:46 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/13 16:03:42 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,10 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
 	if (!s1)
 		return ('\0');
-	return (ft_strncmp(s1, s2, n) ? 0 : 1);
+	if (ft_strncmp(s1,s2,n))
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
