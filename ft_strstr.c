c57071a58e7490fcdcd69ec08effe5d9635b8e4f
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-13 12:00:36 +0200

    ft_strstr.c

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 16:49:51 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/12 17:45:53 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/13 11:59:45 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,20 @@
 char	*ft_strstr(const char *s1, const char *s2)
 {
 	unsigned int	i;
-	unsigned int	k;
+	unsigned int	j;
 
 	i = 0;
-	k = 0;
 	if (s1[0] == '\0' && s2[0] == '\0')
 	{
 		return ((char *)s1);
 	}
 	while (s1[i] != '\0')
 	{
-		while (s2[k] != '\0' && s1[i + k] == s2[k])
-		{
-			k++;
-		}
-		if (s2[k] == '\0')
-		{
+		j = 0;
+		while (s2[j] != '\0' && s1[i + j] == s2[j])
+			j++;
+		if (s2[j] == '\0')
 			return ((char *)s1 + i);
-		}
 		i++;
 	}
 	return (NULL);
