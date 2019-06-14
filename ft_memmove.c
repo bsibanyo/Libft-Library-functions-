724b0591577b8cf285344553f32f29f73491937c
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-14 10:53:18 +0200

    ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 14:21:29 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/11 10:47:07 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/14 10:52:25 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,24 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*ptr;
+	char	*d;
+	char	*s;
+	size_t	i;
 
-	ptr = (char *)malloc(sizeof(char) * len);
-	if (ptr == NULL)
-	{
+	i = 0;
+	d = (char*)dst;
+	s = (char*)src;
+	if (!d && !s)
 		return (NULL);
+	if (s < d)
+		while (++i <= len)
+			d[len - i] = s[len - i];
+	else
+	{
+		while (len-- > 0)
+		{
+			*(d++) = *(s++);
+		}
 	}
-	ft_memcpy(ptr, src, len);
-	ft_memcpy(dst, ptr, len);
-	free(ptr);
 	return (dst);
 }
