6d33692b12e39021da57704347f66bd62a67c98d
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-14 12:07:29 +0200

    ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,9 +6,11 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 16:50:49 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/12 17:49:14 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/14 12:07:12 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 #include "libft.h"
 
@@ -16,15 +18,19 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 	size_t	j;
+	char *p1;
 
 	i = 0;
+	p1 = (char *)s1;
+	if (s2[i] == '\0')
+		return (p1);
 	while (i < n && s1[i] != '\0')
 	{
 		j = 0;
 		while (i + j < n && s1[i + j] == s2[j] && s2[j] != '\0')
 			j++;
 		if (s2[j] == '\0')
-			return ((char *)s1 + i);
+			return (p1 + i);
 		i++;
 	}
 	return (NULL);
