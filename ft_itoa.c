c0c840fe1de03e0a8bddd8df1e5a559b2fb29f8a
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-18 14:58:42 +0200

    ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,21 +6,11 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 16:35:49 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/18 14:49:51 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/18 14:57:53 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static size_t		get_number_length(int n)
-{
-	size_t			i;
-
-	i = 1;
-	while (n /= 10)
-		i++;
-	return (i);
-}
 
 char				*ft_itoa(int n)
 {
@@ -28,7 +18,7 @@ char				*ft_itoa(int n)
 	size_t			i;
 	unsigned int	number;
 
-	i = get_number_length(n);
+	i = ft_get_number_length(n);
 	number = n;
 	if (n < 0)
 	{
@@ -37,9 +27,9 @@ char				*ft_itoa(int n)
 	}
 	if (!(str = ft_strnew(i)))
 		return (NULL);
-	s[--i] = number % 10 + '0';
+	str[--i] = number % 10 + '0';
 	while (number /= 10)
-		s[--i] = number % 10 + '0';
+		str[--i] = number % 10 + '0';
 	if (n < 0)
 		*(str + 0) = '-';
 	return (str);
