5484c7ba0d57b088286ef9e3e5693cabbab8a386
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-20 08:32:23 +0200

    ft_strclr.c

--- ft_strclr.c
+++ ft_strclr.c
@@ -6,7 +6,7 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 15:03:10 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/13 14:51:52 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/20 08:32:08 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	ft_strclr(char *s)
 {
+	unsigned int	i;
+
 	if (!s)
 		return ;
-	unsigned int		i;
-
 	i = 0;
 	while (s[i] != '\0')
 	{
