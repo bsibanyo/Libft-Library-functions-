f0d54135d5df539e830bb28c277bc96f98433c0c
Bruce SIBANYONI <bsibanyo@c5r6s10.wethinkcode.co.za>
2019-06-21 20:08:19 +0200

    ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,11 +6,12 @@
 /*   By: bsibanyo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 13:46:25 by bsibanyo          #+#    #+#             */
-/*   Updated: 2019/06/12 17:20:51 by bsibanyo         ###   ########.fr       */
+/*   Updated: 2019/06/21 20:04:09 by bsibanyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "stdio.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
@@ -33,4 +34,13 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		dst[i + j] = '\0';
 	}
 	return (i + ft_strlen(src));
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		printf("system:%zu\n", strlcat(argv[1], argv[2], strlen(argv[1])));
+		printf("ft_:%zu", ft_strlcat(argv[1], argv[2],ft_strlen(argv[1])));
+	}
 }
